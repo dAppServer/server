628beaefe1363d62e688ba9305934821da4a6a90
Snider <snider@lt.hn>
2021-11-26 17:21:23 +0000

    zeromq websocket for ipc and job queue, with stdIn/stOut websocket to each process managed with ProcessManager

--- src/services/tcp/rest.service.ts
+++ src/services/tcp/rest.service.ts
@@ -5,165 +5,173 @@ import * as path from 'https://deno.land/std/path/mod.ts';
 import os from 'https://deno.land/x/dos@v0.11.0/mod.ts';
 import {LetheanCli} from '../../lethean-cli.ts';
 import {Filter} from '../console-to-html.service.ts';
+import {ZeroMQServer} from '../ipc/zeromq/server.ts';
 import {WebsocketServer} from './websocket.server.ts';
 
+
+
 export class RestService {
-  static app = createApp();
+	static app = createApp();
 
-  static home: string = (os.homeDir() ? os.homeDir() : '') as string;
+	static home: string = (os.homeDir() ? os.homeDir() : '') as string;
 
-  static pathPerms: any = {
-    backend: false,
-    filesystem: true,
-    daemon: true,
-    update: true,
-    help: false,
-    completions: false,
-  };
+	static pathPerms: any = {
+		backend: false,
+		filesystem: true,
+		daemon: true,
+		update: true,
+		help: false,
+		completions: false
+	};
 
-  /**
-   * Recursive tree scan of known commands adding addRoute() to each one found
-   *
-   * @param {string} base
-   * @param routes
-   */
-  static discoverRoute(base: string, routes: any) {
-    for (const dat of routes) {
-      const key = dat[0], value = dat[1];
-      if (
-        RestService.pathPerms[key] === undefined ||
-        RestService.pathPerms[key] !== false
-      ) {
-        console.log(`Adding route: ${[base, key].join("/")}`);
-        this.addRoute([base, key].join("/"), value);
-        if (value.commands) {
-          this.discoverRoute([base, key].join("/"), value.commands);
-        }
-      }
-    }
-  }
+	/**
+	 * Recursive tree scan of known commands adding addRoute() to each one found
+	 *
+	 * @param {string} base
+	 * @param routes
+	 */
+	static discoverRoute(base: string, routes: any) {
+		for (const dat of routes) {
+			const key = dat[0], value = dat[1];
+			if (
+				RestService.pathPerms[key] === undefined ||
+				RestService.pathPerms[key] !== false
+			) {
+				console.log(`Adding route: ${[base, key].join('/')}`);
+				this.addRoute([base, key].join('/'), value);
+				if (value.commands) {
+					this.discoverRoute([base, key].join('/'), value.commands);
+				}
+			}
+		}
+	}
 
-  /**
-   * Creates the cors enabled HTTPS Endpoint for the path and attaches the run() function
-   *
-   * GET - Will trigger the --help docs for the path and display a HTML page
-   * POST - Will trigger the run() function and convert the POST data to commands
-   *
-   * @param {string} path url path
-   * @param handle
-   */
-  static addRoute(path: string, handle: any) {
-    /**
-     * setup the help documentation
-     */
-    this.app.get(path, async (req) => {
-      await req.respond({
-        status: 200,
-        headers: new Headers({
-          "content-type": "text/html",
-          "Access-Control-Allow-Origin": "*",
-        }),
-        body: RestService.templateOutput(handle.getHelp()),
-      });
-    });
+	/**
+	 * Creates the cors enabled HTTPS Endpoint for the path and attaches the run() function
+	 *
+	 * GET - Will trigger the --help docs for the path and display a HTML page
+	 * POST - Will trigger the run() function and convert the POST data to commands
+	 *
+	 * @param {string} path url path
+	 * @param handle
+	 */
+	static addRoute(path: string, handle: any) {
+		/**
+		 * setup the help documentation
+		 */
+		this.app.get(path, async (req) => {
+			await req.respond({
+				status: 200,
+				headers: new Headers({
+					'content-type': 'text/html',
+					'Access-Control-Allow-Origin': '*'
+				}),
+				body: RestService.templateOutput(handle.getHelp())
+			});
+		});
 
-    /**
-     * Setup the action runner
-     */
-    this.app.post(path, async (req) => {
-      const cmdArgs = req.url.replace("/", "").split("/");
+		/**
+		 * Setup the action runner
+		 */
+		this.app.post(path, async (req) => {
+			const cmdArgs = req.url.replace('/', '').split('/');
 
-      const payload = await req.json();
-      for (const key in payload) {
-        const value = payload[key].length > 1 ? `=${payload[key]}` : "";
-        cmdArgs.push(
-          "--" + key.replace(/([A-Z])/g, (x: string) => "-" + x.toLowerCase()) +
-            value,
-        );
-      }
-      try {
-        await LetheanCli.run(cmdArgs);
-        // to send a response throw new StringResponse()
-      } catch (error) {
-        return await req.respond({
-          status: 200,
-          headers: new Headers({
-            "content-type": "text/plain",
-            "Access-Control-Allow-Origin": "*",
-          }),
-          body: error.message,
-        });
-      }
-    });
+			const payload = await req.json();
+			for (const key in payload) {
+				const value = payload[key].length > 1 ? `=${payload[key]}` : '';
+				cmdArgs.push(
+					'--' + key.replace(/([A-Z])/g, (x: string) => '-' + x.toLowerCase()) +
+					value
+				);
+			}
+			try {
+				await LetheanCli.run(cmdArgs);
+				// to send a response throw new StringResponse()
+			} catch (error) {
+				return await req.respond({
+					status: 200,
+					headers: new Headers({
+						'content-type': 'text/plain',
+						'Access-Control-Allow-Origin': '*'
+					}),
+					body: error.message
+				});
+			}
+		});
 
-  }
+	}
 
-  /**
-   * Bootstraps the REST Router
-   */
-  static loadRoutes(){
-    Deno.env.set("REST", "1");
-    this.discoverRoute("", LetheanCli.options.commands);
+	/**
+	 * Bootstraps the REST Router
+	 */
+	static loadRoutes() {
+		Deno.env.set('REST', '1');
+		this.discoverRoute('', LetheanCli.options.commands);
 
-    this.app.handle("/", async (req) => {
-      await req.respond({
-        status: 200,
-        headers: new Headers({
-          "content-type": "text/html",
-          "Access-Control-Allow-Origin": "https://localhost",
-        }),
-        body: RestService.templateOutput(LetheanCli.options.getHelp()),
-      });
-    });
+		this.app.handle('/', async (req) => {
+			await req.respond({
+				status: 200,
+				headers: new Headers({
+					'content-type': 'text/html',
+					'Access-Control-Allow-Origin': 'https://localhost'
+				}),
+				body: RestService.templateOutput(LetheanCli.options.getHelp())
+			});
+		});
 
-  }
+	}
 
-  /**
-   * Start TLS HTTP Rest Server & ZeroMQ Websocket
-   */
-  public static run() {
+	/**
+	 * Start TLS HTTP Rest Server & ZeroMQ Websocket
+	 */
+	public static run() {
 
-    this.loadRoutes()
+		this.loadRoutes();
 
-    this.app.use(cors({
-      origin: "*",
-    }));
+		this.app.use(cors({
+			origin: '*'
+		}));
 
-    if (Deno.readFileSync(path.join(RestService.home, "Lethean", "conf", "private.pem"))) {
-      console.log(`Localhost SSL Found: ${path.join(RestService.home, "Lethean", "conf", "private.pem")}`)
-    } else {
-      console.log('No localhost ssl cert found, injecting a pre made one so we can start a tls server and fix this')
-      RestService.injectPem();
-    }
 
-    this.app.listenTls({
-      "hostname": "localhost",
-      "port": 36911,
-      "certFile": `${path.join(RestService.home, "Lethean", "conf", "public.pem")}`,
-      "keyFile": `${path.join(RestService.home, "Lethean", "conf", "private.pem")}`
-    });
-    WebsocketServer.init()
-    WebsocketServer.startServer()
-  }
+		if (Deno.readFileSync(path.join(RestService.home, 'Lethean', 'conf', 'private.pem'))) {
+			console.log(`Localhost SSL Found: ${path.join(RestService.home, 'Lethean', 'conf', 'private.pem')}`);
+		} else {
+			console.log('No localhost ssl cert found, injecting a pre made one so we can start a tls server and fix this');
+			RestService.injectPem();
+		}
 
-  static templateOutput(input: string) {
-    return new Filter().toHtml(
-      `<html><head></head><body  style="background: radial-gradient(circle,#08f2b5 0%,#158297 100%); "><pre style=" margin-left: 2vw; width: 96vw; background: rgb(33, 33, 33);">${input}</pre></body></html>`,
-    );
-  }
+		this.app.listenTls({
+			'hostname': 'localhost',
+			'port': 36911,
+			'certFile': `${path.join(RestService.home, 'Lethean', 'conf', 'public.pem')}`,
+			'keyFile': `${path.join(RestService.home, 'Lethean', 'conf', 'private.pem')}`
+		});
 
-  public static config() {
-    return new Command()
-      .description("Backend Services for Application GUI")
-      .command("start", "Start Application Helper Daemon")
-      .action(() => RestService.run());
-  }
+		ZeroMQServer.startServer();
+		WebsocketServer.startServer();
+	}
 
-  private static injectPem(){
-    const home = os.homeDir();
-    Deno.mkdirSync(path.join(home ? home : '~', "Lethean", "conf"))
+	static templateOutput(input: string) {
+		return new Filter().toHtml(
+			`<html><head></head><body  style="background: radial-gradient(circle,#08f2b5 0%,#158297 100%); "><pre style=" margin-left: 2vw; width: 96vw; background: rgb(33, 33, 33);">${input}</pre></body></html>`
+		);
+	}
 
-     Deno.writeTextFileSync(path.join(home ? home : '~', "Lethean", "conf", "private.pem"), `-----BEGIN RSA PRIVATE KEY-----
+	public static config() {
+		return new Command()
+			.description('Backend Services for Application GUI')
+			.command('start', 'Start Application Helper Daemon')
+			.action(() => RestService.run())
+			.command('ipc')
+			.description('Start ZeroMQ server')
+			.action(() => ZeroMQServer.startServer());
+	}
+
+	private static injectPem() {
+		const home = os.homeDir();
+		Deno.mkdirSync(path.join(home ? home : '~', 'Lethean', 'conf'));
+
+		Deno.writeTextFileSync(path.join(home ? home : '~', 'Lethean', 'conf', 'private.pem'), `-----BEGIN RSA PRIVATE KEY-----
 MIIEogIBAAKCAQEAqX/7sHcFXtk5fvfeAMU+m+zuiF6IegEef0NrwaaYvxlpC0I6
 vcksaHNFzAu0KArDwow8n+xtRg+07UXSdphO+D0tnHHpfamESPysz4Ik3q8cVnQM
 AqHXy5yaJBFK/hm521MvmwHubTVnyhJcUSR1/1OIbyPZ23P32P7bc/Tg+ELOSIWR
@@ -190,7 +198,7 @@ Je1RAoGAZV96nHg4PNJWMHeJs8yYFCpeE6dC3J4gK2aR3P9dNbXkIDBhc/6dnR0O
 /rqKx/VXYqpnOeJBHDc8ImMsPAOBigc8pAHfR4GsXKw3frQqFQkz5XuAZZHWl1Bw
 YOFbSYitBYVg9X650Vx9UP358H3SpLjmaTW4ZgdQnJgZAw3Eoqk=
 -----END RSA PRIVATE KEY-----`);
-     Deno.writeTextFileSync(path.join(home ? home : '~', "Lethean", "conf", "public.pem"), `-----BEGIN CERTIFICATE-----
+		Deno.writeTextFileSync(path.join(home ? home : '~', 'Lethean', 'conf', 'public.pem'), `-----BEGIN CERTIFICATE-----
 MIICpDCCAYwCCQDkJzAtbfDzOTANBgkqhkiG9w0BAQsFADAUMRIwEAYDVQQDDAls
 b2NhbGhvc3QwHhcNMjExMTEzMTcwMjI2WhcNMzAwMTMwMTcwMjI2WjAUMRIwEAYD
 VQQDDAlsb2NhbGhvc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCp
@@ -208,5 +216,5 @@ ItWiOjl5IlU4F4E/VenI/HViuZb22svPeZQPFU0K/3NZthEc9q8fpqH2cK/U2rbk
 iN0Y4jlcEAI=
 -----END CERTIFICATE-----`);
 
-  }
+	}
 }
