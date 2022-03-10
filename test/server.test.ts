e6a2df23c7bb181f0a67a1e488e371b01241760f
Snider <snider@lt.hn>
2022-03-10 15:30:06 +0000

    gh action test report

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,7 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.129.0/testing/asserts.ts";
-
+import { LetheanCli } from "../src/lethean-cli.ts";
 // Compact form: name and function
-Deno.test("hello world #1", () => {
+Deno.test("hello world #1", async () => {
   const x = 1 + 2;
+  await LetheanCli.init();
   assertEquals(x, 3);
 });
