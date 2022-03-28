81dfbfcfb182821cf9cfaf3f617cc19f94a937cf
Snider <snider@lt.hn>
2022-03-28 19:46:08 +0100

    if DEV env variable is "true" use angular dev server not lethean-server to show native GUI

--- src/services/display/gui.ts
+++ src/services/display/gui.ts
@@ -22,7 +22,13 @@ export class LetheanGUI {
    start(app = 'desktop') {
     console.info(`Loading App GUI: ${app}`)
     this.webview.title = `Lethean ${app}`
-    this.webview.navigate(`http://127.0.0.1:36911/app/${app}/`);
+
+    if(Deno.env.get('DEV') == 'true') {
+      this.webview.navigate(`http://127.0.0.1:4200/`);
+    } else {
+      this.webview.navigate(`http://127.0.0.1:36911/app/${app}/`);
+    }
+
     this.webview.run();
     return true;
   }
