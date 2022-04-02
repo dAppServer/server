cc12bb26f5ad8682b2a937a77d44f63808c0c802
Snider <snider@lt.hn>
2022-04-02 12:33:28 +0100

    removes a dev file, wasnt a "test"

--- test/server.test.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.129.0/testing/asserts.ts";
-import { LetheanCli } from "../src/lethean-cli.ts";
-// Compact form: name and function
-Deno.test("hello world #1", async () => {
-  const x = 1 + 2;
-  await LetheanCli.init();
-  assertEquals(x, 3);
-});
