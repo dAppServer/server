b9a091c606176341e08240fd4a88d0096d9f3722
Snider <snider@lt.hn>
2022-04-02 11:59:29 +0100

    adds user create and delete test

--- test/auth/user.test.ts
+++ test/auth/user.test.ts
@@ -4,12 +4,18 @@ import { LetheanAccount } from "../../src/accounts/user.ts";
 import { FilesystemService } from "../../src/services/filesystem.service.ts";
 import { QuasiSalt } from "../../src/services/crypt/quasi-salt.ts";
 
-Deno.test("Create a Lethean Account, check user files created", async () => {
+Deno.test('Create a Lethean Account, check user files created, check user files deleted', async () => {
 
   const key: any = await LetheanAccount.create("test", "test")
 
   assertEquals(FilesystemService.existsFile({ path: `users/${QuasiSalt.hash('test')}.lthn.pub` }), true)
   assertEquals(FilesystemService.existsFile({ path: `users/${QuasiSalt.hash('test')}.lthn.key` }), true)
   assertEquals(FilesystemService.existsFile({ path: `users/${QuasiSalt.hash('test')}.lthn.rev` }), true)
+
+  await LetheanAccount.delete("test")
+
+  assertEquals(FilesystemService.existsFile({ path: `users/${QuasiSalt.hash('test')}.lthn.pub` }), false)
+  assertEquals(FilesystemService.existsFile({ path: `users/${QuasiSalt.hash('test')}.lthn.key` }), false)
+  assertEquals(FilesystemService.existsFile({ path: `users/${QuasiSalt.hash('test')}.lthn.rev` }), false)
 
 });
