f38f386c4a6acd25d33da72b477de4739b867a72
Snider <snider@lt.hn>
2022-04-02 19:21:04 +0100

    adds a test for getPublicKeyById

--- test/auth/openpgp.test.ts
+++ test/auth/openpgp.test.ts
@@ -1,6 +1,7 @@
-import {  assertStringIncludes } from "https://deno.land/std@0.129.0/testing/asserts.ts";
+import {assertEquals,  assertStringIncludes } from "https://deno.land/std@0.129.0/testing/asserts.ts";
 
 import { CryptOpenPGP } from "../../src/services/crypt/openpgp.ts";
+import { LetheanAccount } from "../../src/accounts/user.ts";
 
 Deno.test("Create OpenPGP keys, check if looks like a key", async () => {
 
@@ -9,4 +10,11 @@ Deno.test("Create OpenPGP keys, check if looks like a key", async () => {
   assertStringIncludes(key.privateKey, '-----BEGIN PGP PRIVATE KEY BLOCK-----', "Private key is not valid")
   assertStringIncludes(key.publicKey, '-----BEGIN PGP PUBLIC KEY BLOCK-----', "Public key is not valid")
   assertStringIncludes(key.revocationCertificate, '-----BEGIN PGP PUBLIC KEY BLOCK-----', "Revocation certificate is not valid")
+});
+
+Deno.test('getPublicKeyById', async () => {
+  const key: any = await LetheanAccount.create("test2", "test")
+  const publicKey = await CryptOpenPGP.getPublicKey('09f3b17c6fab2d948fb29a5ab9aaf9755fd24c3c146ed19795a85c446241ad89')
+
+  assertEquals( await publicKey.getPrimaryUser().then(data => data.user.userID.userID), '09f3b17c6fab2d948fb29a5ab9aaf9755fd24c3c146ed19795a85c446241ad89', "OpenPGP Key is not for the test user")
 });
