d8ab3cad71fddd20ef34dc883ddb61db8d41c7dc
Snider <snider@lt.hn>
2022-04-04 16:02:45 +0100

    updates vendor files

--- vendor/deno.land/x/superoak@4.7.0/version.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Version of SuperOak.
- */
-export const VERSION = "4.7.0";
-
-/**
- * Supported versions of Deno.
- */
-export const DENO_SUPPORTED_VERSIONS = ["1.19.3"];
--- vendor/jspm.dev/npm_has@1!cjs.js
+++
@@ -1,2 +0,0 @@
-import "/npm:function-bind@1!cjs";
-export { default } from "/npm:has@1.0.3!cjs";
