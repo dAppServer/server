32fb9fb731f2e2cb93f0fca9036dccd2f6e15ad2
Snider <snider@lt.hn>
2022-07-08 14:29:47 +0100

    xmrig updates

--- src/modules/mining/xmrig/xmrig.controller.ts
+++ src/modules/mining/xmrig/xmrig.controller.ts
@@ -50,6 +50,22 @@ XmrigRouter.post("/mining/xmrig/start", async (context: Context) => {
 
     exeFile = FileSystemService.path(["cli", "xmrig", "xmrig-6.18.0", exeFile]);
 
+    req['config'] = FileSystemService.path(["conf", "xmrig", "config.json"]);
+
+    if(req['logFile']){
+      req['logFile'] = FileSystemService.path(req['logFile']);
+    }
+
+    req['httpPort'] = "36979"
+    //req['cpuMaxThreadsHint'] = '50'
+//    args['httpPort'] = '36979'
+//    args['httpHost'] = 'localhost' --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+    req['httpAccessToken'] = 'dave1wasneverhere'
+    if(req['url'].startsWith('pool.hashvault.pro')){
+      //req['tls'] = true
+      //req['tlsFingerprint'] = '420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14'
+    }
+
     ProcessManager.run(
       exeFile,
       req,
@@ -65,5 +81,39 @@ XmrigRouter.post("/mining/xmrig/start", async (context: Context) => {
     throw new HttpException("Not Found", 404);
   }
 });
+
+XmrigRouter.post("/mining/xmrig/api", async (context: Context) => {
+  const body = context.request.body({ type: "json" });
+  const req = await body.value;
+  let endpoint = "/2/summary";
+  if (req["endpoint"]) {
+    endpoint = req["endpoint"];
+  }
+  let method = "GET"
+  if (req["method"]) {
+    method = req["method"];
+  }
+  try {
+    // console.log(url, req)
+    const postReq = await fetch(
+      `http://127.0.0.1:36979${endpoint}`,
+      {
+        method: method,
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer dave1wasneverhere"
+        },
+        body: JSON.stringify(req['request']),
+      },
+    );
+    context.response.status = 200;
+    context.response.body = await postReq.text();
+    // console.log(context.response.body)
+  } catch (error) {
+    return false;
+  }
+});
+
+
 
 export { XmrigRouter };
