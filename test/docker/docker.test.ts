8482a0967a360d4172a95b4d0a5ffc09eca6f9b1
Snider <snider@lt.hn>
2022-10-22 16:00:03 +0100

    disabling docker module tests

--- test/docker/docker.test.ts
+++ test/docker/docker.test.ts
@@ -1,24 +1,24 @@
-import { path } from "../../deps.ts";
-import { assertEquals } from "../../deps-test.ts";
-import { DockerService } from "../../src/modules/docker/docker.service.ts";
-
-Deno.test("DockerService.listContainers", async () => {
-  try {
-
-    const docker = new DockerService();
-    const containers = await docker.listContainers();
-    assertEquals(containers[0].Id != undefined, true);
-  } catch (e) {
-
-  }
-});
-
-Deno.test("DockerService.createContainer", async () => {
-  try {
-    const docker = new DockerService();
-    const container = await docker.createContainer("LetheanTest", { Image: "lthn/chain:latest" });
-    console.log(container);
-  } catch (e) {
-
-  }
-});
+//import { path } from "../../deps.ts";
+//import { assertEquals } from "../../deps-test.ts";
+//import { DockerService } from "../../src/modules/docker/docker.service.ts";
+//
+//Deno.test("DockerService.listContainers", async () => {
+//  try {
+//
+//    const docker = new DockerService();
+//    const containers = await docker.listContainers();
+//    assertEquals(containers[0].Id != undefined, true);
+//  } catch (e) {
+//
+//  }
+//});
+//
+//Deno.test("DockerService.createContainer", async () => {
+//  try {
+//    const docker = new DockerService();
+//    const container = await docker.createContainer("LetheanTest", { Image: "lthn/chain:latest" });
+//    console.log(container);
+//  } catch (e) {
+//
+//  }
+//});
