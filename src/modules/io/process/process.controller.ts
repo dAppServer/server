30d730c746100e4fe71df1dfaef9cf5e330819ae
Snider <snider@lt.hn>
2023-03-07 12:58:37 +0000

    code docs

--- src/modules/io/process/process.controller.ts
+++ src/modules/io/process/process.controller.ts
@@ -7,30 +7,51 @@ import { ProcessAddDTO, ProcessKillDTO, ProcessRunDTO, ProcessStartDTO, ProcessS
 export class ProcessController  {
   constructor(private process: ProcessManager) {}
 
-
+  /**
+   * Run a process
+   * @param {ProcessRunDTO} body
+   * @returns {ProcessManagerProcess | Promise<void>}
+   */
   @Post("run")
   runProcess(@Body() body: ProcessRunDTO) {
     return this.process.run(body.command, body.args, body.options);
   }
 
 
+  /**
+   * Add a process to process registry
+   * @param {ProcessAddDTO} body
+   * @returns {ProcessManagerProcess}
+   */
   @Post("add")
   addProcess(@Body() body: ProcessAddDTO) {
     return this.process.addProcess(body);
   }
 
 
+  /**
+   * Start a process in the process registry
+   * @param {ProcessStartDTO} body
+   */
   @Post("start")
   startProcess(@Body() body: ProcessStartDTO) {
     return this.process.startProcess(body.key);
   }
 
+  /**
+   * Stop a process in the process registry
+   * @param {ProcessStopDTO} body
+   */
   @Post("stop")
   stopProcess(@Body() body: ProcessStopDTO) {
     return this.process.stopProcess(body.key);
   }
 
 
+  /**
+   * Kill a process in the process registry
+   * @param {ProcessKillDTO} body
+   */
   @Post("kill")
   killProcess(@Body() body: ProcessKillDTO) {
     return this.process.killProcess(body.key);
