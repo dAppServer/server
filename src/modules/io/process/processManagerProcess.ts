52bc6b11b512b30b0a17e863bd616cf3d98243f5
Snider <snider@lt.hn>
2023-04-12 17:38:29 +0100

    upgrades ReadLines

--- src/modules/io/process/processManagerProcess.ts
+++ src/modules/io/process/processManagerProcess.ts
@@ -1,7 +1,7 @@
 import { ProcessManagerRequest } from "/modules/io/process/process.interface.ts";
 import { Sub } from "https://deno.land/x/jszmq@v1.3.1/mod.ts";
 import { ZeroMQServer } from "/modules/io/ipc/zeromq.ts";
-import { readLines } from "/deps.ts";
+import { readLines } from "std/io/read_lines.ts";
 import { EventEmitter } from "node:events";
 
 /**
