4fec812ee0f0fc72a009eb7f44f4caf9204835e8
Snider <snider@lt.hn>
2023-06-13 19:17:06 +0100

    windows init fix

--- mod.ts
+++ mod.ts
@@ -3,9 +3,9 @@ import  * as path from "std/path/mod.ts";
 
 const home = Deno.env.get('HOME') ? Deno.env.get('HOME') as string : Deno.cwd();
 
-if(path.join(home,'Lethean') !== Deno.cwd()){
-  Deno.chdir(path.join(home,'Lethean'))
-}
+// if(path.join(home,'Lethean') !== Deno.cwd()){
+//   Deno.chdir(path.join(home,'Lethean'))
+// }
 
 import { bootstrap } from './src/bootstrap.ts';
 
