ed2fc7db213f112e06939a627f177eb1154cafc7
Snider <snider@lt.hn>
2024-01-01 14:51:54 +0000

    Updates server folder to .dappserver, now creates directory and cd's to pwd() + '.dappserver'

--- mod.ts
+++ mod.ts
@@ -1,11 +1,23 @@
-
+#!/usr/bin/env -S deno run --allow-net --allow-read --allow-write --allow-env --unstable
 import  * as path from "std/path/mod.ts";
+/**
+ * @env HOME - The home directory of the user or server sandbox directory.
+ * @env PORT - The port to listen on, default 36911.
+ */
+const startDir = Deno.env.get('HOME') ? Deno.env.get('HOME') as string : Deno.cwd();
+console.log('startDir',startDir)
+if(!Deno.cwd().endsWith('.dappserver')){
+    try {
+        if(Deno.statSync(path.join(startDir,'.dappserver')).isDirectory){
+           // console.log('DappServer installed')
+        }
+    } catch (e) {
+        Deno.mkdirSync(path.join(startDir,'.dappserver'))
+    }
 
-const home = Deno.env.get('HOME') ? Deno.env.get('HOME') as string : Deno.cwd();
-
-// if(path.join(home,'Lethean') !== Deno.cwd()){
-//   Deno.chdir(path.join(home,'Lethean'))
-// }
+   Deno.chdir(path.join(startDir,'.dappserver'))
+    console.log('Server Dir', Deno.cwd())
+}
 
 import { bootstrap } from './src/bootstrap.ts';
 
