df6e8877c45f4a50c0e6cbd769916adf7b8ec3b6
Snider <snider@lethean.io>
2024-02-03 12:32:00 +0000

    http status codes

---
+++ src/interfaces/http.ts
@@ -0,0 +1,63 @@
+import { ApiProperty, Optional } from "danetSwagger/decorators.ts";
+import { HTTP_STATUS } from "@interfaces/status-codes.ts";
+
+
+export class ServerResponse {
+    @ApiProperty()
+    status!: HTTP_STATUS;
+    
+    @ApiProperty()
+    data!: string;
+
+    @ApiProperty()
+    @Optional()
+    signature?: string;
+
+    constructor(status: HTTP_STATUS, data: string) {
+        this.status = status;
+        this.data = data;
+        //this.signature = ;
+    }
+}
+
+export class EncryptedRequestDTO {
+  @ApiProperty()
+  payload!: string;
+
+  @ApiProperty()
+  @Optional()
+  signature?: string;
+}
+
+export class EncryptedResponseDTO {
+  @ApiProperty()
+  result!: string;
+
+  @ApiProperty()
+  @Optional()
+  signature?: string;
+}
+
+export class DeleteAccountDTO {
+  @ApiProperty()
+  username!: string;
+
+  @ApiProperty()
+  password!: string;
+}
+
+export class CreateAccountDTO {
+  @ApiProperty()
+  username!: string;
+
+  @ApiProperty()
+  password!: string;
+}
+
+export class CreateAccountResponseDTO {
+  @ApiProperty()
+  userhash!: string;
+
+  @ApiProperty()
+  pubkey!: string;
+}
