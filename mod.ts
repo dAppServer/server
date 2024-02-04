5a9ae4118911c556389cdb4d3139caaa32bcb61c
Snider <snider@lethean.io>
2024-02-04 16:41:29 +0000

    Openapi spec builder, pass --openapi and the server will create a openapi.json document in the working directory then exit.

--- mod.ts
+++ mod.ts
@@ -1,13 +1,20 @@
-
-import  * as path from "std/path/mod.ts";
-
-const home = Deno.env.get('HOME') ? Deno.env.get('HOME') as string : Deno.cwd();
+//import  * as path from "std/path/mod.ts";
+import { bootstrap } from "./src/bootstrap.ts";
+import { buildSpec } from "./src/openapi.ts";
+//const home = Deno.env.get('HOME') ? Deno.env.get('HOME') as string : Deno.cwd();
 
 // if(path.join(home,'Lethean') !== Deno.cwd()){
 //   Deno.chdir(path.join(home,'Lethean'))
 // }
 
-import { bootstrap } from './src/bootstrap.ts';
-
-const application = await bootstrap();
-await application.listen(Number(Deno.env.get('PORT') || 36911));
+if (Deno.args.length > 0) {
+  if (Deno.args.find((arg) => arg === "--openapi")) {
+    console.log("Swagger API Docs + JSON Definition");
+    Deno.writeTextFileSync("openapi.json", JSON.stringify(await buildSpec()));
+    console.log(`openapi.json created in dir: ${Deno.cwd()}`);
+  }
+} else {
+  const application = await bootstrap();
+  await application.listen(Number(Deno.env.get("PORT") || 36911));
+}
+Deno.exit(0);
---
+++ src/openapi.ts
@@ -0,0 +1,24 @@
+import { AppModule } from "./app.module.ts";
+import { DanetApplication } from "danet/mod.ts";
+import { SpecBuilder, SwaggerModule } from "danetSwagger/mod.ts";
+export const buildSpec = async () => {
+  const application = new DanetApplication();
+  // LoggerMiddleware must be the first middleware
+  // TimingMiddleware must be the second middleware
+  // application.addGlobalMiddlewares(LoggerMiddleware,TimingMiddleware, CorsMiddleware, RequestIDMiddleware, JWTAuthMiddleware);
+  application.enableCors();
+  await application.init(AppModule);
+
+  // Swagger API Docs + JSON Definition
+  const spec = new SpecBuilder()
+    .setTitle("Lethean Server")
+    .setDescription("Lethean dAppServer")
+    .setVersion("3.1.1")
+    .addServer("http://localhost:36911")
+    .setContact("Lethean", "https://lethean.io", "hello@lt.hn")
+    .setLicense("EUPL-1.2", "https://eupl.eu/1.2/en/")
+    .build();
+  const document = await SwaggerModule.createDocument(application, spec);
+
+  return document;
+};
