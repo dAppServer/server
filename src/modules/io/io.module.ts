03ddba76058d070caf022d6d47e34ce2ef242381
Snider <snider@lethean.io>
2024-02-15 19:53:47 +0000

    proc control removed from default feature set.

--- src/modules/io/io.module.ts
+++ src/modules/io/io.module.ts
@@ -3,19 +3,15 @@ import { FileSystemService } from "@module/io/filesystem/fileSystemService.ts";
 import { FileSystemController } from "@module/io/filesystem/local.controller.ts";
 import { DownloadController } from "@module/io/tcp/download.controller.ts";
 import { LetheanDownloadService } from "@module/io/tcp/download.service.ts";
-import { ProcessController } from "@module/io/process/process.controller.ts";
-import { ProcessManager } from "@module/io/process/process.service.ts";
 
 @Module({
   controllers: [
     FileSystemController,
-    DownloadController,
-    ProcessController
+    DownloadController
   ],
   injectables: [
     FileSystemService,
-    LetheanDownloadService,
-    ProcessManager
+    LetheanDownloadService
   ],
 })
 export class IOModule {}
