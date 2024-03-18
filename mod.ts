8396779f4fad4399386696513037dbb42b73d698
Snider <snider@lethean.io>
2024-03-18 15:18:57 +0000

    adds cli interface, server now starts with the server command, default is to exit;

--- mod.ts
+++ mod.ts
@@ -1,22 +1,52 @@
 //import  * as path from "std/path/mod.ts";
 import { bootstrap } from "./src/bootstrap.ts";
 import { buildSpec } from "./src/openapi.ts";
-
+import { Command, HelpCommand, CompletionsCommand, UpgradeCommand, GithubProvider, DenoLandProvider } from "https://deno.land/x/cliffy/command/mod.ts";
+import packageFile from "./package.json" with { type: "json" };
 //const home = Deno.env.get('HOME') ? Deno.env.get('HOME') as string : Deno.cwd();
 
 // if(path.join(home,'Lethean') !== Deno.cwd()){
 //   Deno.chdir(path.join(home,'Lethean'))
 // }
+await new Command()
+    .name('lthn')
+    .version(packageFile.version)
+    .command(
+        "openapi",
+        "Creates OpenAPI definition json file.",
+    )
+    .action(async (_) => {
+      // Disable console.log to avoid printing the entire spec to the console
+      const origLog = console.log;
+      console.log = function() {
+        return;
+      };
+      Deno.writeTextFileSync("openapi.json", JSON.stringify(await buildSpec()));
+      console.log = origLog;
+      console.log(`openapi.json created in dir: ${Deno.cwd()}`);
+      Deno.exit(0);
+    })
+    .command(
+        "start",
+        "Starts the server.",
+    )
+    .action(async (_) => {
+      const application = await bootstrap();
+      await application.listen(Number(Deno.env.get("PORT") || 36911));
+    })
+    .command("help", new HelpCommand().global())
+    .command("completions", new CompletionsCommand())
+    .command(
+        "upgrade",
+        new UpgradeCommand({
+          main: "mod.ts",
+          args: ["--allow-all", "--unstable"],
+          provider: [
+              //new DenoLandProvider({ name: "lthn"}),
+              new GithubProvider({ repository: "dappserver/server" }),
+          ],
+        }),
+    )
+    .parse(Deno.args);
 
-if (Deno.args.length > 0) {
-  if (Deno.args.find((arg) => arg === "--openapi")) {
-    console.log("Swagger API Docs + JSON Definition");
-    Deno.writeTextFileSync("openapi.json", JSON.stringify(await buildSpec()));
-    console.log(`openapi.json created in dir: ${Deno.cwd()}`);
-    Deno.exit(0);
-  }
-} else {
-  const application = await bootstrap();
-  await application.listen(Number(Deno.env.get("PORT") || 36911));
-}
-
+Deno.exit(0);
