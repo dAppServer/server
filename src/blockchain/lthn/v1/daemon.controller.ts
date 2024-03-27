cb7a0eca7cc06b56e8e14e72c0686f30faf3a808
Snider <snider@lethean.io>
2024-03-27 17:19:19 +0000

    adds lthn daemon control

--- src/blockchain/lthn/v1/daemon.controller.ts
+++ src/blockchain/lthn/v1/daemon.controller.ts
@@ -1,13 +1,48 @@
-import {Controller} from "https://deno.land/x/danet/mod.ts";
+import {Body, Controller, Logger} from "https://deno.land/x/danet/mod.ts";
 import {Get, Post} from "https://deno.land/x/danet_swagger@2.0.0/deps.ts";
 import {ProcessService} from "../../../mod/process/process.service.ts";
-import {Tag} from "https://deno.land/x/danet_swagger@2.0.0/decorators.ts";
+import {ApiProperty, ReturnedType, Tag} from "https://deno.land/x/danet_swagger@2.0.0/decorators.ts";
+import {ModIoFsLocalService} from "../../../mod/io/fs/local/service.ts";
+import {ServerResponse} from "../../../interfaces/http.ts";
+import { LetheanDownloadService } from "../../../mod/io/protocols/http/download/client.service.ts";
+import * as path from "https://deno.land/x/std/path/mod.ts";
+import {IniService} from "../../../mod/config/ini/ini.service.ts";
+export class BlockchainLetheanDaemonStartDTO {
+    @ApiProperty()
+    configFile: string;
+    @ApiProperty()
+    dataDir: string;
+    @ApiProperty()
+    logDir: string;
 
+}
+
+export class BlockchainLetheanRPCDTO {
+    @ApiProperty()
+    url: string;
+    @ApiProperty()
+    req: string;
+
+}
+
+export class BlockchainLetheanWalletStartDTO {
+    @ApiProperty()
+    walletDir: string;
+    @ApiProperty()
+    rpcBindPort: number;
+    @ApiProperty()
+    disableRpcLogin: boolean;
+}
 @Controller("/api/blockchain/lthn/v1/daemon")
 @Tag('blockchain-lthn-v1-daemon')
 export class BlockchainLTHNV1DaemonController {
+    log: Logger;
 
-    constructor(private process: ProcessService) {
+    constructor(private process: ProcessService,
+                private fileSystem: ModIoFsLocalService,
+                private download: LetheanDownloadService,
+                private ini: IniService) {
+        this.log = new Logger('LetheanDaemonController');
     }
 
     @Get('')
@@ -17,10 +52,119 @@ export class BlockchainLTHNV1DaemonController {
         };
     }
 
+    /**
+     * @description Start the Lethean Blockchain Daemon
+     */
     @Post()
-    async startDaemon() {
-        const { code } =  await this.process.start('letheand' )
+    async start() {
+        const { code } =  await this.process.start('apps/blockchain/lthn/letheand' )
         return { code, msg: "Daemon started" }
+    }
+
+    @Post("start")
+    @ReturnedType(ServerResponse)
+    startDaemon(@Body() body: BlockchainLetheanDaemonStartDTO): ServerResponse {
+
+        let cmd: any = {};
+
+        const configFile = this.fileSystem.path(path.join("conf", "lthn", body.configFile));
+
+        if (!this.fileSystem.isFile(path.join("conf", "lthn", body.configFile))) {
+            this.log.log(`Config file ${configFile} not found`);
+            if (!this.fileSystem.isDir(path.join("conf", "lthn"))) {
+                this.fileSystem.ensureDir(path.join("conf", "lthn"));
+            }
+            this.fileSystem.write(
+                configFile,
+                this.ini.stringify({
+                    "log-file": body.logDir
+                })
+            );
+        }
+        cmd["configFile"] = configFile;
+        if (!this.fileSystem.isDir(body.dataDir)) {
+            this.fileSystem.ensureDir(body.dataDir);
+        }
+
+        if (!this.fileSystem.isDir(body.logDir)) {
+            this.fileSystem.ensureDir(body.logDir);
+        }
+
+        const exeFile = path.join("apps", 'blockchain', 'lthn',`letheand${Deno.build.os === "windows" ? ".exe" : ""}`);
+
+        if (!this.fileSystem.isFile(exeFile)) {
+            this.log.error(`Lethean Daemon executable not found: ${exeFile}`);
+            return new ServerResponse(404, "Lethean Daemon executable not found, please install the daemon");
+        }
+
+        this.process.start(
+            this.fileSystem.path(exeFile),
+            cmd
+        );
+        return new ServerResponse(200, "Daemon started");
+
+    }
+
+    @Post("json_rpc")
+    @ReturnedType(String)
+    async jsonRpc(@Body() body: BlockchainLetheanRPCDTO): Promise<string> {
+        let url = "json_rpc";
+        if (body["url"]) {
+            url = body["url"];
+        }
+
+        const postReq = await fetch(
+            `http://127.0.0.1:48782/${url}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: body["req"]
+            }
+        );
+        return await postReq.text();
+
+    }
+
+    @Post("export")
+    exportBlockchain(@Body() body: any) {
+        const exeFile = `lethean-blockchain-export${
+            Deno.build.os === "windows" ? ".exe" : ""
+        }`;
+        this.process.start(
+            this.fileSystem.path(["apps", 'blockchain', 'lthn', exeFile]),
+            body
+        );
+    }
+
+    @Post("import")
+    importBlockchain(@Body() body: any) {
+        const exeFile = `lethean-blockchain-import${
+            Deno.build.os === "windows" ? ".exe" : ""
+        }`;
+        this.process.start(
+            this.fileSystem.path(["apps", 'blockchain', 'lthn', exeFile]),
+            body
+        );
+    }
+
+    @Post("download")
+    downloadDaemon(@Body() body: any) {
+        const urls = {
+            'linux': 'https://github.com/letheanVPN/blockchain-iz/releases/latest/download/lethean-cli-linux.tar',
+            'windows': 'https://github.com/letheanVPN/blockchain-iz/releases/latest/download/lethean-cli-windows.tar',
+            'mac': 'https://github.com/letheanVPN/blockchain-iz/releases/latest/download/lethean-cli-macos.tar'
+        }
+        if(urls[Deno.build.os]){
+            this.download.downloadContents(urls[Deno.build.os], 'apps/blockchain/lthn').catch((err) => {
+                console.log(err);
+            });
+            return {status: 'Download Requested'};
+        }else{
+            return {status: 'OS not supported'};
+        }
+
     }
     //
     // @Delete()
