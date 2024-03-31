003c670d02a995b8bccaff0ba8dbeec0ac2d8290
Snider <snider@lethean.io>
2024-03-31 18:19:32 +0100

    fixes testcase for process.service.ts

--- tests/process/cmd-ls.test.ts
+++ tests/process/cmd-ls.test.ts
@@ -3,7 +3,7 @@ import {ProcessService} from "../../src/mod/process/process.service.ts";
 
 const process = new ProcessService();
 Deno.test("ProcessService.start('ls')", async () => {
-    const { code, stdout, stderr } = await process.start('ls', ['-l', '.'])
+    const { code, stdout, stderr } = await process.run('ls', ['-l', '.'])
     assertEquals(code, 0);
     assertEquals(new TextDecoder().decode(stdout).includes("mod.ts"), true);
 });
@@ -13,8 +13,8 @@ Deno.test("Process Persistence", async () => {
     process.kill('ls')
     assertEquals(process.list().includes("ls"), false);
 });
-Deno.test("ProcessService.start('pwd')", async () => {
-    const { code, stdout, stderr } = await process.start('pwd')
+Deno.test("ProcessService.run('pwd')", async () => {
+    const { code, stdout, stderr } = await process.run('pwd')
     assertEquals(code, 0);
     assertEquals(new TextDecoder().decode(stdout).includes("server"), true);
 });
